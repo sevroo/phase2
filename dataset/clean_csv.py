d4d57d18ce7367e715eaca6e3c03c4abcc6b8971
Christine <cumberbinch@gmail.com>
2020-10-30 23:21:16 -0400

    fix typo

--- dataset/clean_csv.py
+++ dataset/clean_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 # read steam csv file
 df = pd.read_csv('steam.csv')
 # limit to games with less than 60 characters
-# df = orig_df[orig_df['name'].map(len) <= 60]
+# df = df[df['name'].map(len) <= 60]
 
 # take midpoint of owners
 owners = df['owners']
